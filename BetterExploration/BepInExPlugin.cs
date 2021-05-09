6e237964d054e60d4fd4b7572f75af857026910d
Daniel Schulz <faulolio@gmail.com>
2021-05-09 15:49:27 +0200

    Add BetterExploration stub (thanks https://www.youtube.com/watch?v=p_gsFASlvRw)

---
+++ BetterExploration/BepInExPlugin.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Slothsoft.BetterExploration {
+    [BepInPlugin("Slothsoft.BetterExploration", "Better Exploration", "0.0.1")]
+    public partial class BepInExPlugin : BaseUnityPlugin {
+        static ConfigEntry<bool> modEnabled;
+
+        void Awake() {
+            modEnabled = Config.Bind("General", "Enabled", true, "Enable this mod");
+
+            Debug.Log($"{typeof(BepInExPlugin)} enabled: {modEnabled.Value}");
+
+            if (!modEnabled.Value) {
+                return;
+            }
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+        }
+
+        [HarmonyPatch(typeof(Character), nameof(Character.Jump))]
+        static class CharacterJumpPatch {
+            static void Prefix(ref float jumpForce) {
+                if (!modEnabled.Value) {
+                    return;
+                }
+
+                jumpForce *= 2;
+                Debug.Log($"Increased jumpForce to '{jumpForce}'!");
+            }
+        }
+    }
+}
